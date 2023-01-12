225a86fe50829ce3ea759c23b11284463c8012e5
yps <1106728884@qq.com>
2023-01-12 16:53:41 +0800

    setup改造

--- dist/vue-diy-rightmenu-plus.mjs
+++ dist/vue-diy-rightmenu-plus.mjs
@@ -1,12 +1,9 @@
-import { ref as S, watch as M, onBeforeUnmount as L, resolveComponent as B, openBlock as n, createElementBlock as o, withModifiers as V, renderSlot as D, normalizeStyle as O, Fragment as v, renderList as _, createBlock as y, withCtx as C, resolveDynamicComponent as g, normalizeClass as k, toDisplayString as p, createCommentVNode as h, createTextVNode as w, createElementVNode as z } from "vue";
-const A = (l, i) => {
-  const s = l.__vccOpts || l;
-  for (const [e, f] of i)
-    s[e] = f;
-  return s;
-}, N = {
-  name: "vueDiyRightmenuPlus",
-  emits: ["menuClick", "popShow", "popClose"],
+import { ref as h, watch as M, onBeforeUnmount as V, resolveComponent as z, openBlock as t, createElementBlock as l, withModifiers as A, renderSlot as D, normalizeStyle as N, Fragment as r, renderList as _, createBlock as d, withCtx as x, resolveDynamicComponent as S, normalizeClass as m, toDisplayString as p, createCommentVNode as C, createTextVNode as E, createElementVNode as O } from "vue";
+const $ = ["onContextmenu"], P = {
+  key: 0,
+  class: "menu-list"
+}, j = ["onClick"], F = { class: "item-title" }, R = ["onClick"], T = {
+  __name: "index",
   props: {
     groupList: {
       type: Array,
@@ -35,122 +32,114 @@ const A = (l, i) => {
       default: !0
     }
   },
-  setup(l, { attrs: i, slots: s, emit: e, expose: f }) {
-    const r = S(!1), m = S({
+  emits: ["menuClick", "popShow", "popClose"],
+  setup(a, { emit: i }) {
+    const s = a, u = h(!1), y = h({
       left: "9999px",
       top: "9999px"
-    }), t = S({
-      name: l.props.name || "name",
-      data: l.props.data || "data",
-      label: l.props.label || "label",
-      value: l.props.value || "value",
-      icon: l.props.icon || "icon"
-    }), u = (a, x) => {
-      let E = a.matches || a.webkitMatchesSelector || a.mozMatchesSelector || a.msMatchesSelector;
-      for (; a && !E.call(a, x); )
-        a = a.parentElement;
-      return a;
-    }, c = () => {
-      u(event.target, ".menu-content") === null && (m.value.left = event.clientX + "px", m.value.top = event.clientY + "px", r.value = !0, e("popShow"));
-    }, d = () => {
-      (l.hideOnClick || u(event.target, ".menu-content") === null) && (r.value = !1, e("popClose"));
+    }), e = h({
+      name: s.props.name || "name",
+      data: s.props.data || "data",
+      label: s.props.label || "label",
+      value: s.props.value || "value",
+      icon: s.props.icon || "icon"
+    }), g = (n, b) => {
+      let v = n.matches || n.webkitMatchesSelector || n.mozMatchesSelector || n.msMatchesSelector;
+      for (; n && !v.call(n, b); )
+        n = n.parentElement;
+      return n;
+    }, L = () => {
+      g(event.target, ".menu-content") === null && (y.value.left = event.clientX + "px", y.value.top = event.clientY + "px", u.value = !0, i("popShow"));
+    }, k = () => {
+      (s.hideOnClick || g(event.target, ".menu-content") === null) && (u.value = !1, i("popClose"));
     };
-    return M(r, () => {
-      r.value ? document.body.addEventListener("click", d) : document.body.removeEventListener("click", d);
-    }), L(() => {
-      document.body.removeEventListener("click", d);
-    }), {
-      pop: r,
-      popStyle: m,
-      nameSet: t,
-      showMenu: c,
-      menuClick: (a) => {
-        e("menuClick", a);
-      }
+    M(u, () => {
+      u.value ? document.body.addEventListener("click", k) : document.body.removeEventListener("click", k);
+    }), V(() => {
+      document.body.removeEventListener("click", k);
+    });
+    const w = (n) => {
+      i("menuClick", n);
+    };
+    return (n, b) => {
+      const v = z("el-icon");
+      return t(), l("div", {
+        class: "vue-diy-rightmenu-plus",
+        onContextmenu: A(L, ["prevent"])
+      }, [
+        D(n.$slots, "default"),
+        u.value ? (t(), l("div", {
+          key: 0,
+          class: "menu-content",
+          style: N(y.value)
+        }, [
+          a.list.length > 0 ? (t(), l("div", P, [
+            (t(!0), l(r, null, _(a.list, (o, f) => (t(), l("div", {
+              class: "item",
+              onClick: (c) => w(o[e.value.value]),
+              key: f
+            }, [
+              o[e.value.icon] ? (t(), l(r, { key: 0 }, [
+                o[e.value.icon].split("/")[0] === "el" ? (t(), d(v, { key: 0 }, {
+                  default: x(() => [
+                    (t(), d(S(o[e.value.icon].split("/")[1])))
+                  ]),
+                  _: 2
+                }, 1024)) : o[e.value.icon].split("/")[0] === "iconfont" ? (t(), l("i", {
+                  key: 1,
+                  class: m([
+                    "icon iconfont",
+                    "icon-" + o[e.value.icon].split("/")[1]
+                  ])
+                }, null, 2)) : (t(), l("i", {
+                  key: 2,
+                  class: m(o[e.value.icon].split("/")[0])
+                }, p(o[e.value.icon].split("/")[1]), 3))
+              ], 64)) : C("", !0),
+              E(" " + p(o[e.value.label]), 1)
+            ], 8, j))), 128))
+          ])) : (t(!0), l(r, { key: 1 }, _(a.groupList, (o, f) => (t(), l("div", {
+            class: "menu-group-list",
+            key: f
+          }, [
+            O("p", F, p(o[e.value.name]), 1),
+            (t(!0), l(r, null, _(o[e.value.data], (c, B) => (t(), l("div", {
+              class: "item",
+              onClick: (Y) => w(c[e.value.value]),
+              key: B
+            }, [
+              c[e.value.icon] ? (t(), l(r, { key: 0 }, [
+                c[e.value.icon].split("/")[0] === "el" ? (t(), d(v, { key: 0 }, {
+                  default: x(() => [
+                    (t(), d(S(c[e.value.icon].split("/")[1])))
+                  ]),
+                  _: 2
+                }, 1024)) : c[e.value.icon].split("/")[0] === "iconfont" ? (t(), l("i", {
+                  key: 1,
+                  class: m([
+                    "icon iconfont",
+                    "icon-" + c[e.value.icon].split("/")[1]
+                  ])
+                }, null, 2)) : (t(), l("i", {
+                  key: 2,
+                  class: m(c[e.value.icon].split("/")[0])
+                }, p(c[e.value.icon].split("/")[1]), 3))
+              ], 64)) : C("", !0),
+              E(" " + p(c[e.value.label]), 1)
+            ], 8, R))), 128))
+          ]))), 128))
+        ], 4)) : C("", !0)
+      ], 40, $);
     };
   }
-}, P = {
-  key: 0,
-  class: "menu-list"
-}, R = ["onClick"], j = { class: "item-title" }, F = ["onClick"];
-function T(l, i, s, e, f, r) {
-  const m = B("el-icon");
-  return n(), o("div", {
-    class: "vue-diy-rightmenu-plus",
-    onContextmenu: i[0] || (i[0] = V((...t) => e.showMenu && e.showMenu(...t), ["prevent"]))
-  }, [
-    D(l.$slots, "default"),
-    e.pop ? (n(), o("div", {
-      key: 0,
-      class: "menu-content",
-      style: O(e.popStyle)
-    }, [
-      s.list.length > 0 ? (n(), o("div", P, [
-        (n(!0), o(v, null, _(s.list, (t, u) => (n(), o("div", {
-          class: "item",
-          onClick: (c) => e.menuClick(t[e.nameSet.value]),
-          key: u
-        }, [
-          t[e.nameSet.icon] ? (n(), o(v, { key: 0 }, [
-            t[e.nameSet.icon].split("/")[0] === "el" ? (n(), y(m, { key: 0 }, {
-              default: C(() => [
-                (n(), y(g(t[e.nameSet.icon].split("/")[1])))
-              ]),
-              _: 2
-            }, 1024)) : t[e.nameSet.icon].split("/")[0] === "iconfont" ? (n(), o("i", {
-              key: 1,
-              class: k([
-                "icon iconfont",
-                "icon-" + t[e.nameSet.icon].split("/")[1]
-              ])
-            }, null, 2)) : (n(), o("i", {
-              key: 2,
-              class: k(t[e.nameSet.icon].split("/")[0])
-            }, p(t[e.nameSet.icon].split("/")[1]), 3))
-          ], 64)) : h("", !0),
-          w(" " + p(t[e.nameSet.label]), 1)
-        ], 8, R))), 128))
-      ])) : (n(!0), o(v, { key: 1 }, _(s.groupList, (t, u) => (n(), o("div", {
-        class: "menu-group-list",
-        key: u
-      }, [
-        z("p", j, p(t[e.nameSet.name]), 1),
-        (n(!0), o(v, null, _(t[e.nameSet.data], (c, d) => (n(), o("div", {
-          class: "item",
-          onClick: (b) => e.menuClick(c[e.nameSet.value]),
-          key: d
-        }, [
-          c[e.nameSet.icon] ? (n(), o(v, { key: 0 }, [
-            c[e.nameSet.icon].split("/")[0] === "el" ? (n(), y(m, { key: 0 }, {
-              default: C(() => [
-                (n(), y(g(c[e.nameSet.icon].split("/")[1])))
-              ]),
-              _: 2
-            }, 1024)) : c[e.nameSet.icon].split("/")[0] === "iconfont" ? (n(), o("i", {
-              key: 1,
-              class: k([
-                "icon iconfont",
-                "icon-" + c[e.nameSet.icon].split("/")[1]
-              ])
-            }, null, 2)) : (n(), o("i", {
-              key: 2,
-              class: k(c[e.nameSet.icon].split("/")[0])
-            }, p(c[e.nameSet.icon].split("/")[1]), 3))
-          ], 64)) : h("", !0),
-          w(" " + p(c[e.nameSet.label]), 1)
-        ], 8, F))), 128))
-      ]))), 128))
-    ], 4)) : h("", !0)
-  ], 32);
-}
-const U = /* @__PURE__ */ A(N, [["render", T]]), X = [U], Y = {
-  install(l) {
-    X.forEach((i) => {
-      l.component(i.name, i);
+}, U = [T], X = {
+  install(a) {
+    U.forEach((i) => {
+      a.component("vueDiyRightmenuPlus", i);
     });
   }
 };
-typeof window < "u" && window.Vue && window.Vue.use(Y);
+typeof window < "u" && window.Vue && window.Vue.use(X);
 export {
-  Y as default
+  X as default
 };
--- lib/index.vue
+++ lib/index.vue
@@ -4,7 +4,7 @@
 */
 /*
  * @LastEditors: aFei
- * @LastEditTime: 2022-11-23 09:28:34
+ * @LastEditTime: 2023-01-12 16:50:00
 */
 <template>
   <div class="vue-diy-rightmenu-plus" @contextmenu.prevent="showMenu">
@@ -14,23 +14,15 @@
       <!-- 普通按钮形式 -->
       <template v-if="list.length > 0">
         <div class="menu-list">
-          <div
-            class="item"
-            @click="menuClick(item[nameSet.value])"
-            v-for="(item, index) in list"
-            :key="index"
-          >
+          <div class="item" @click="menuClick(item[nameSet.value])" v-for="(item, index) in list" :key="index">
             <template v-if="item[nameSet.icon]">
               <el-icon v-if="item[nameSet.icon].split('/')[0] === 'el'">
                 <component :is="item[nameSet.icon].split('/')[1]" />
               </el-icon>
-              <i
-                :class="[
-                  'icon iconfont',
-                  'icon-' + item[nameSet.icon].split('/')[1],
-                ]"
-                v-else-if="item[nameSet.icon].split('/')[0] === 'iconfont'"
-              />
+              <i :class="[
+                'icon iconfont',
+                'icon-' + item[nameSet.icon].split('/')[1],
+              ]" v-else-if="item[nameSet.icon].split('/')[0] === 'iconfont'" />
               <i :class="item[nameSet.icon].split('/')[0]" v-else>{{
                 item[nameSet.icon].split("/")[1]
               }}</i>
@@ -41,29 +33,18 @@
       </template>
       <!-- 按钮组形式 -->
       <template v-else>
-        <div
-          class="menu-group-list"
-          v-for="(item, index) in groupList"
-          :key="index"
-        >
+        <div class="menu-group-list" v-for="(item, index) in groupList" :key="index">
           <p class="item-title">{{ item[nameSet.name] }}</p>
-          <div
-            class="item"
-            @click="menuClick(one[nameSet.value])"
-            v-for="(one, oneIndex) in item[nameSet.data]"
-            :key="oneIndex"
-          >
+          <div class="item" @click="menuClick(one[nameSet.value])" v-for="(one, oneIndex) in item[nameSet.data]"
+            :key="oneIndex">
             <template v-if="one[nameSet.icon]">
               <el-icon v-if="one[nameSet.icon].split('/')[0] === 'el'">
                 <component :is="one[nameSet.icon].split('/')[1]" />
               </el-icon>
-              <i
-                :class="[
-                  'icon iconfont',
-                  'icon-' + one[nameSet.icon].split('/')[1],
-                ]"
-                v-else-if="one[nameSet.icon].split('/')[0] === 'iconfont'"
-              />
+              <i :class="[
+                'icon iconfont',
+                'icon-' + one[nameSet.icon].split('/')[1],
+              ]" v-else-if="one[nameSet.icon].split('/')[0] === 'iconfont'" />
               <i :class="one[nameSet.icon].split('/')[0]" v-else>{{
                 one[nameSet.icon].split("/")[1]
               }}</i>
@@ -75,125 +56,113 @@
     </div>
   </div>
 </template>
-<script>
+<script setup>
 import { onBeforeUnmount, watch, ref } from "vue";
-export default {
-  name: "vueDiyRightmenuPlus",
-  emits: ["menuClick", "popShow", "popClose"],
+const emit = defineEmits(["menuClick", "popShow", "popClose"]);
+const props = defineProps({
+  // 按钮组数据
+  groupList: {
+    type: Array,
+    default: () => {
+      return [];
+    },
+  },
+  // 按钮数据
+  list: {
+    type: Array,
+    default: () => {
+      return [
+        {
+          label: "编辑",
+          value: "编辑value",
+          icon: "el/Apple",
+        },
+        {
+          label: "删除",
+          value: "删除value",
+        },
+      ];
+    },
+  },
+  // 按钮键值配置
   props: {
-    // 按钮组数据
-    groupList: {
-      type: Array,
-      default: () => {
-        return [];
-      },
-    },
-    // 按钮数据
-    list: {
-      type: Array,
-      default: () => {
-        return [
-          {
-            label: "编辑",
-            value: "编辑value",
-            icon: "el/Apple",
-          },
-          {
-            label: "删除",
-            value: "删除value",
-          },
-        ];
-      },
-    },
-    // 按钮键值配置
-    props: {
-      type: Object,
-      default: () => {
-        return {};
-      },
-    },
-    // 点击按钮后是否隐藏弹窗
-    hideOnClick: {
-      type: Boolean,
-      default: true,
+    type: Object,
+    default: () => {
+      return {};
     },
   },
-  setup(props, { attrs, slots, emit, expose }) {
-    const pop = ref(false);
-    const popStyle = ref({
-      left: "9999px",
-      top: "9999px",
-    });
-    const nameSet = ref({
-      name: props.props.name || "name",
-      data: props.props.data || "data",
-      label: props.props.label || "label",
-      value: props.props.value || "value",
-      icon: props.props.icon || "icon",
-    });
-    // 查找最近的一个符合条件的元素
-    const closest = (ele, selector) => {
-      let matchesSelector =
-        ele.matches ||
-        ele.webkitMatchesSelector ||
-        ele.mozMatchesSelector ||
-        ele.msMatchesSelector;
-      while (ele) {
-        if (matchesSelector.call(ele, selector)) {
-          break;
-        }
-        ele = ele.parentElement;
-      }
-      return ele;
-    };
-    // 右键事件
-    const showMenu = () => {
-      console.log(event, 1111);
-      if (closest(event.target, ".menu-content") === null) {
-        console.log("未点击在弹窗上");
-        popStyle.value.left = event.clientX + "px";
-        popStyle.value.top = event.clientY + "px";
-        pop.value = true;
-        emit("popShow");
-      } else {
-        console.log("点击在弹窗上");
-      }
-    };
-    // 隐藏弹窗
-    const hidePop = () => {
-      console.log("触发检测函数");
-      if (
-        props.hideOnClick ||
-        closest(event.target, ".menu-content") === null
-      ) {
-        pop.value = false;
-        emit("popClose");
-      }
-    };
-    // 监听弹窗出现
-    watch(pop, () => {
-      if (pop.value) {
-        document.body.addEventListener("click", hidePop);
-      } else {
-        document.body.removeEventListener("click", hidePop);
-      }
-    });
-    onBeforeUnmount(() => {
-      document.body.removeEventListener("click", hidePop);
-    });
-    // 按钮点击事件
-    const menuClick = (value) => {
-      console.log(value);
-      emit("menuClick", value);
-    };
-    return {
-      pop,
-      popStyle,
-      nameSet,
-      showMenu,
-      menuClick,
-    };
-  },
+  // 点击按钮后是否隐藏弹窗
+  hideOnClick: {
+    type: Boolean,
+    default: true,
+  }
+});
+const pop = ref(false);
+const popStyle = ref({
+  left: "9999px",
+  top: "9999px",
+});
+const nameSet = ref({
+  name: props.props.name || "name",
+  data: props.props.data || "data",
+  label: props.props.label || "label",
+  value: props.props.value || "value",
+  icon: props.props.icon || "icon",
+});
+// 查找最近的一个符合条件的元素
+const closest = (ele, selector) => {
+  let matchesSelector =
+    ele.matches ||
+    ele.webkitMatchesSelector ||
+    ele.mozMatchesSelector ||
+    ele.msMatchesSelector;
+  while (ele) {
+    if (matchesSelector.call(ele, selector)) {
+      break;
+    }
+    ele = ele.parentElement;
+  }
+  return ele;
+};
+// 右键事件
+const showMenu = () => {
+  console.log(event, 1111);
+  if (closest(event.target, ".menu-content") === null) {
+    console.log("未点击在弹窗上");
+    popStyle.value.left = event.clientX + "px";
+    popStyle.value.top = event.clientY + "px";
+    pop.value = true;
+    emit("popShow");
+  } else {
+    console.log("点击在弹窗上");
+  }
+};
+// 隐藏弹窗
+const hidePop = () => {
+  console.log("触发检测函数");
+  if (
+    props.hideOnClick ||
+    closest(event.target, ".menu-content") === null
+  ) {
+    pop.value = false;
+    emit("popClose");
+  }
+};
+// 监听弹窗出现
+watch(pop, () => {
+  if (pop.value) {
+    document.body.addEventListener("click", hidePop);
+  } else {
+    document.body.removeEventListener("click", hidePop);
+  }
+});
+onBeforeUnmount(() => {
+  document.body.removeEventListener("click", hidePop);
+});
+// 按钮点击事件
+const menuClick = (value) => {
+  console.log(value);
+  emit("menuClick", value);
 };
 </script>
 <style lang="scss">
