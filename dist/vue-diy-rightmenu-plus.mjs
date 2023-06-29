047145d34e3644afe351e2f0986361a86d3fc06c
yps <1106728884@qq.com>
2023-06-29 17:21:01 +0800

    代码简化

--- dist/vue-diy-rightmenu-plus.mjs
+++ dist/vue-diy-rightmenu-plus.mjs
@@ -1,139 +1,146 @@
-import { ref as C, watch as P, onBeforeUnmount as z, resolveComponent as D, openBlock as o, createElementBlock as l, withModifiers as N, renderSlot as O, unref as e, normalizeStyle as $, Fragment as y, renderList as w, createBlock as u, resolveDynamicComponent as k, normalizeProps as f, mergeProps as s, withCtx as L, toDisplayString as v, createCommentVNode as b, createTextVNode as B, createElementVNode as A } from "vue";
-const j = ["onContextmenu"], F = {
+import { resolveComponent as M, openBlock as t, createBlock as d, resolveDynamicComponent as w, normalizeProps as x, mergeProps as m, withCtx as V, createElementBlock as i, toDisplayString as p, ref as k, watch as P, onBeforeUnmount as _, withModifiers as $, renderSlot as z, unref as n, normalizeStyle as D, Fragment as f, renderList as h, createCommentVNode as O, createTextVNode as S, createElementVNode as N } from "vue";
+const E = {
+  __name: "icon",
+  props: {
+    iconObj: {
+      type: Object,
+      requured: !0
+    }
+  },
+  setup(e) {
+    return (r, s) => {
+      const a = M("el-icon");
+      return e.iconObj.type === "custom" ? (t(), d(w(e.iconObj.icon), x(m({ key: 0 }, e.iconObj.attrs)), null, 16)) : e.iconObj.type === "el" ? (t(), d(a, x(m({ key: 1 }, e.iconObj.attrs)), {
+        default: V(() => [
+          (t(), d(w(e.iconObj.icon)))
+        ]),
+        _: 1
+      }, 16)) : e.iconObj.type === "iconfont" ? (t(), i("i", m({
+        key: 2,
+        class: ["icon iconfont", "icon-" + e.iconObj.icon]
+      }, e.iconObj.attrs), null, 16)) : (t(), i("i", m({
+        key: 3,
+        class: e.iconObj.type
+      }, e.iconObj.attrs), p(e.iconObj.icon), 17));
+    };
+  }
+};
+const A = ["onContextmenu"], q = {
   key: 0,
   class: "menu-list"
-}, R = ["onClick"], T = { class: "item-title" }, U = ["onClick"], X = {
+}, F = ["onClick"], R = { class: "item-title" }, T = ["onClick"], U = {
   __name: "index",
   props: {
+    // 按钮组数据
     groupList: {
       type: Array,
       default: () => []
     },
+    // 按钮数据
     list: {
       type: Array,
       default: () => [
         {
-          label: "\u7F16\u8F91",
-          value: "\u7F16\u8F91value",
-          icon: { icon: "\u221A" }
+          label: "编辑",
+          value: "编辑value",
+          icon: { icon: "√" }
         },
         {
-          label: "\u5220\u9664",
-          value: "\u5220\u9664value"
+          label: "删除",
+          value: "删除value"
         }
       ]
     },
+    // 按钮键值配置
     props: {
       type: Object,
       default: () => ({})
     },
+    // 点击按钮后是否隐藏弹窗
     hideOnClick: {
       type: Boolean,
       default: !0
     }
   },
   emits: ["menuClick", "popShow", "popClose"],
-  setup(a, { emit: p }) {
-    const r = a, d = C(!1), h = C({
+  setup(e, { emit: r }) {
+    const s = e, a = k(!1), v = k({
       left: "9999px",
       top: "9999px"
-    }), n = C({
-      name: r.props.name || "name",
-      data: r.props.data || "data",
-      label: r.props.label || "label",
-      value: r.props.value || "value",
-      icon: r.props.icon || "icon"
-    }), x = (i, E) => {
-      let m = i.matches || i.webkitMatchesSelector || i.mozMatchesSelector || i.msMatchesSelector;
-      for (; i && !m.call(i, E); )
-        i = i.parentElement;
-      return i;
-    }, M = () => {
-      x(event.target, ".menu-content") === null && (h.value.left = event.clientX + "px", h.value.top = event.clientY + "px", d.value = !0, p("popShow"));
-    }, _ = () => {
-      (r.hideOnClick || x(event.target, ".menu-content") === null) && (d.value = !1, p("popClose"));
+    }), o = k({
+      name: s.props.name || "name",
+      data: s.props.data || "data",
+      label: s.props.label || "label",
+      value: s.props.value || "value",
+      icon: s.props.icon || "icon"
+    }), j = (c, C) => {
+      let l = c.matches || c.webkitMatchesSelector || c.mozMatchesSelector || c.msMatchesSelector;
+      for (; c && !l.call(c, C); )
+        c = c.parentElement;
+      return c;
+    }, L = () => {
+      j(event.target, ".menu-content") === null && (v.value.left = event.clientX + "px", v.value.top = event.clientY + "px", a.value = !0, r("popShow"));
+    }, y = () => {
+      (s.hideOnClick || j(event.target, ".menu-content") === null) && (a.value = !1, r("popClose"));
     };
-    P(d, () => {
-      d.value ? document.body.addEventListener("click", _) : document.body.removeEventListener("click", _);
-    }), z(() => {
-      document.body.removeEventListener("click", _);
+    P(a, () => {
+      a.value ? document.body.addEventListener("click", y) : document.body.removeEventListener("click", y);
+    }), _(() => {
+      document.body.removeEventListener("click", y);
     });
-    const S = (i) => {
-      p("menuClick", i);
+    const g = (c) => {
+      r("menuClick", c);
     };
-    return (i, E) => {
-      const m = D("el-icon");
-      return o(), l("div", {
-        class: "vue-diy-rightmenu-plus",
-        onContextmenu: N(M, ["prevent"])
+    return (c, C) => (t(), i("div", {
+      class: "vue-diy-rightmenu-plus",
+      onContextmenu: $(L, ["prevent"])
+    }, [
+      z(c.$slots, "default"),
+      n(a) ? (t(), i("div", {
+        key: 0,
+        class: "menu-content",
+        style: D(n(v))
       }, [
-        O(i.$slots, "default"),
-        e(d) ? (o(), l("div", {
-          key: 0,
-          class: "menu-content",
-          style: $(e(h))
-        }, [
-          a.list.length > 0 ? (o(), l("div", F, [
-            (o(!0), l(y, null, w(a.list, (t, g) => (o(), l("div", {
-              class: "item",
-              onClick: (c) => S(t[e(n).value]),
-              key: g
-            }, [
-              t[e(n).icon] && (t[e(n).icon].icon || t[e(n).icon].type) ? (o(), l(y, { key: 0 }, [
-                t[e(n).icon].type === "custom" ? (o(), u(k(t[e(n).icon].icon), f(s({ key: 0 }, t[e(n).icon].attrs)), null, 16)) : t[e(n).icon].type === "el" ? (o(), u(m, f(s({ key: 1 }, t[e(n).icon].attrs)), {
-                  default: L(() => [
-                    (o(), u(k(t[e(n).icon].icon)))
-                  ]),
-                  _: 2
-                }, 1040)) : t[e(n).icon].type === "iconfont" ? (o(), l("i", s({
-                  key: 2,
-                  class: ["icon iconfont", "icon-" + t[e(n).icon].icon]
-                }, t[e(n).icon].attrs), null, 16)) : (o(), l("i", s({
-                  key: 3,
-                  class: t[e(n).icon].type
-                }, t[e(n).icon].attrs), v(t[e(n).icon].icon), 17))
-              ], 64)) : b("", !0),
-              B(" " + v(t[e(n).label]), 1)
-            ], 8, R))), 128))
-          ])) : (o(!0), l(y, { key: 1 }, w(a.groupList, (t, g) => (o(), l("div", {
-            class: "menu-group-list",
-            key: g
+        e.list.length > 0 ? (t(), i("div", q, [
+          (t(!0), i(f, null, h(e.list, (l, b) => (t(), i("div", {
+            class: "item",
+            onClick: (u) => g(l[n(o).value]),
+            key: b
           }, [
-            A("p", T, v(t[e(n).name]), 1),
-            (o(!0), l(y, null, w(t[e(n).data], (c, V) => (o(), l("div", {
-              class: "item",
-              onClick: (G) => S(c[e(n).value]),
-              key: V
-            }, [
-              c[e(n).icon] && (c[e(n).icon].icon || c[e(n).icon].type) ? (o(), l(y, { key: 0 }, [
-                c[e(n).icon].type === "custom" ? (o(), u(k(c[e(n).icon].icon), f(s({ key: 0 }, c[e(n).icon].attrs)), null, 16)) : c[e(n).icon].type === "el" ? (o(), u(m, f(s({ key: 1 }, c[e(n).icon].attrs)), {
-                  default: L(() => [
-                    (o(), u(k(c[e(n).icon].icon)))
-                  ]),
-                  _: 2
-                }, 1040)) : c[e(n).icon].type === "iconfont" ? (o(), l("i", s({
-                  key: 2,
-                  class: ["icon iconfont", "icon-" + c[e(n).icon].icon]
-                }, c[e(n).icon].attrs), null, 16)) : (o(), l("i", s({
-                  key: 3,
-                  class: c[e(n).icon].type
-                }, c[e(n).icon].attrs), v(c[e(n).icon].icon), 17))
-              ], 64)) : b("", !0),
-              B(" " + v(c[e(n).label]), 1)
-            ], 8, U))), 128))
-          ]))), 128))
-        ], 4)) : b("", !0)
-      ], 40, j);
-    };
+            l[n(o).icon] && (l[n(o).icon].icon || l[n(o).icon].type) ? (t(), d(E, {
+              key: 0,
+              iconObj: l[n(o).icon]
+            }, null, 8, ["iconObj"])) : O("", !0),
+            S(" " + p(l[n(o).label]), 1)
+          ], 8, F))), 128))
+        ])) : (t(!0), i(f, { key: 1 }, h(e.groupList, (l, b) => (t(), i("div", {
+          class: "menu-group-list",
+          key: b
+        }, [
+          N("p", R, p(l[n(o).name]), 1),
+          (t(!0), i(f, null, h(l[n(o).data], (u, B) => (t(), i("div", {
+            class: "item",
+            onClick: (G) => g(u[n(o).value]),
+            key: B
+          }, [
+            u[n(o).icon] && (u[n(o).icon].icon || u[n(o).icon].type) ? (t(), d(E, {
+              key: 0,
+              iconObj: u[n(o).icon]
+            }, null, 8, ["iconObj"])) : O("", !0),
+            S(" " + p(u[n(o).label]), 1)
+          ], 8, T))), 128))
+        ]))), 128))
+      ], 4)) : O("", !0)
+    ], 40, A));
   }
-}, Y = [X], q = {
-  install(a) {
-    Y.forEach((p) => {
-      a.component("vueDiyRightmenuPlus", p);
+}, X = [U], Y = {
+  install(e) {
+    X.forEach((r) => {
+      e.component("vueDiyRightmenuPlus", r);
     });
   }
 };
-typeof window < "u" && window.Vue && window.Vue.use(q);
+typeof window < "u" && window.Vue && window.Vue.use(Y);
 export {
-  q as default
+  Y as default
 };
